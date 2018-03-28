c23e2e8d2f119a077edac93a9d565987420d5b26
Yurnero <muravyoov@gmail.com>
2018-03-28 21:59:27 +0300

    Add migration 'AddOrgNameProp'

--- Migrations/20180328184144_AddOrgNameProp.Designer.cs
+++ Migrations/20180328184144_AddOrgNameProp.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace ApacheLogParser.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20180328184144_AddOrgNameProp")]
+    partial class AddOrgNameProp
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Migrations/20180328184144_AddOrgNameProp.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace ApacheLogParser.Migrations
+{
+    public partial class AddOrgNameProp : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "OrgName",
+                table: "Hosts",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "OrgName",
+                table: "Hosts");
+        }
+    }
+}
