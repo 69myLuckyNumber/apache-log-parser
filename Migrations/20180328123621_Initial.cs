49e7ec171a6cd461ce9aea5961fc318aa76afc5f
Yurnero <muravyoov@gmail.com>
2018-03-28 15:41:38 +0300

    Add migrations

---
+++ Migrations/20180328123621_Initial.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace ApacheLogParser.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Hosts",
+                columns: table => new
+                {
+                    IPAddressBytes = table.Column<byte[]>(maxLength: 16, nullable: false),
+                    HostName = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_IPAddressBytes", x => x.IPAddressBytes);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Requests",
+                columns: table => new
+                {
+                    RequestId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    DateTimeRequested = table.Column<DateTime>(nullable: false),
+                    RequestType = table.Column<string>(nullable: true),
+                    RequestorIPAddress = table.Column<byte[]>(nullable: true),
+                    RequestorIPAddressBytes = table.Column<byte[]>(nullable: true),
+                    ResponseStatusCode = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Requests", x => x.RequestId);
+                    table.ForeignKey(
+                        name: "FK_Requests_Hosts_RequestorIPAddressBytes",
+                        column: x => x.RequestorIPAddressBytes,
+                        principalTable: "Hosts",
+                        principalColumn: "IPAddressBytes",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Files",
+                columns: table => new
+                {
+                    FileId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    FileName = table.Column<string>(nullable: true),
+                    FilePath = table.Column<string>(nullable: true),
+                    FileSize = table.Column<int>(nullable: false),
+                    RequestId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Files", x => x.FileId);
+                    table.ForeignKey(
+                        name: "FK_Files_Requests_RequestId",
+                        column: x => x.RequestId,
+                        principalTable: "Requests",
+                        principalColumn: "RequestId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Files_RequestId",
+                table: "Files",
+                column: "RequestId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Requests_RequestorIPAddressBytes",
+                table: "Requests",
+                column: "RequestorIPAddressBytes");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Files");
+
+            migrationBuilder.DropTable(
+                name: "Requests");
+
+            migrationBuilder.DropTable(
+                name: "Hosts");
+        }
+    }
+}
