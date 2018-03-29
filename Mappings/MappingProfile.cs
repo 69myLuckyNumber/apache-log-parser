2e6035f2bb480ca986499cf259828ad4a3404aa6
Yurnero <muravyoov@gmail.com>
2018-03-29 14:14:34 +0300

    Add corresponding mappings

--- Mappings/MappingProfile.cs
+++ Mappings/MappingProfile.cs
@@ -1,8 +1,10 @@
+using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
 using ApacheLogParser.Core.Abstract;
 using ApacheLogParser.Core.Models;
 using ApacheLogParser.Core.Pocos;
+using ApacheLogParser.Core.ViewModels;
 using AutoMapper;
 
 namespace ApacheLogParser.Mappings
@@ -10,9 +12,11 @@ namespace ApacheLogParser.Mappings
     public class MappingProfile : Profile
     {
         private readonly IHostParser hostParser;
+        private readonly IRepository repository;
 
-        public MappingProfile(IHostParser hostParser)
+        public MappingProfile(IHostParser hostParser, IRepository repository)
         {
+            this.repository = repository;
             this.hostParser = hostParser;
 
             CreateMap<LogEntry, Request>()
@@ -20,23 +24,43 @@ namespace ApacheLogParser.Mappings
                 .ForMember(dest => dest.RequestType, opts => opts.MapFrom(src => src.RequestType))
                 .ForMember(dest => dest.DateTimeRequested, opts => opts.MapFrom(src => src.DateTimeRequested))
                 .ForMember(dest => dest.Requestor, opts => opts.MapFrom(src => GetRequestor(src)))
-                .ForMember(dest => dest.RequestedFile, opts => opts.MapFrom(src => 
-                    new File 
-                    { 
+                .ForMember(dest => dest.RequestorIPAddress, opts => opts.MapFrom(src => Encoding.ASCII.GetBytes(src.IPAddress)))
+                .ForMember(dest => dest.RequestedFile, opts => opts.MapFrom(src =>
+                    new File
+                    {
                         FileName = src.FileName,
-                        FilePath = src.FilePath, 
-                        FileSize = src.BytesSent 
+                            FilePath = src.FilePath,
+                            FileSize = src.BytesSent
                     }));
+                
+
+            CreateMap<Request, RequestViewModel>()
+                .ForMember(dest => dest.RequestType, opts => opts.MapFrom(src => src.RequestType))
+                .ForMember(dest => dest.ResponseStatusCode, opts => opts.MapFrom(src => src.ResponseStatusCode))
+                .ForMember(dest => dest.DateTimeRequested, opts => opts.MapFrom(src => src.DateTimeRequested))
+                .ForMember(dest => dest.FileName, opts => opts.MapFrom(src => src.RequestedFile.FileName))
+                .ForMember(dest => dest.FilePath, opts => opts.MapFrom(src => src.RequestedFile.FilePath))
+                .ForMember(dest => dest.BytesSent, opts => opts.MapFrom(src => src.RequestedFile.FileSize));
+
+            CreateMap<Host, HostViewModel>()
+                .ForMember(dest => dest.IpAddress, opts => opts.MapFrom(src => Encoding.UTF8.GetString(src.IPAddressBytes)))
+                .ForMember(dest => dest.HostName, opts => opts.MapFrom(src => src.HostName))
+                .ForMember(dest => dest.OrgName, opts => opts.MapFrom(src => src.OrgName))
+                .ForMember(dest => dest.Requests, opts => opts.MapFrom(src => src.Requests));
         }
 
         private Host GetRequestor(LogEntry log)
         {
+            var hostIp = Encoding.ASCII.GetBytes(log.IPAddress);
+            if(repository.isHostPresent(hostIp))
+                return null;
+
             var hostInfo = hostParser.ParseIpAddressAsync(log.IPAddress).Result;
-            return new Host 
-            { 
-                IPAddressBytes = Encoding.ASCII.GetBytes(hostInfo.IP) , 
-                HostName = hostInfo.HostName, 
-                OrgName = hostInfo.Org 
+            return new Host
+            {
+                IPAddressBytes = hostIp,
+                HostName = hostInfo.HostName,
+                OrgName = hostInfo.Org
             };
         }
     }
